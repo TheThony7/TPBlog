59692065554dd4eff0cb923cb626c94898afb75f
Anthony <anthony.desa88@gmail.com>
2023-04-20 16:49:07 +0200

    fin

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -2,17 +2,22 @@
 
 namespace App\Controller;
 
+use App\Repository\ArticleRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class HomeController extends AbstractController
 {
-    #[Route('/', name: 'app_home')]
-    public function index(): Response
+    #[Route('/', name: 'app_home', methods: ['GET'])]
+    public function index(
+        ArticleRepository $article
+    ): Response
     {
         return $this->render('home/index.html.twig', [
             'controller_name' => 'HomeController',
+
+        
         ]);
     }
 }
--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -10,7 +10,7 @@ class AppFixtures extends Fixture
 {
     public function load(ObjectManager $manager): void
     {
-        for($i = 0; $i < 50; $i++) {
+        for($i = 1; $i < 50; $i++) {
             // Ajoute des articles à la base de données
             $article = new Article();
             $article->setTitre('Article ' . $i)
